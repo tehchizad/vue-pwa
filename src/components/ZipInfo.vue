0be72813db12225002973926fe989597f83d92d2
The Chad <tehchizad@gmail.com>
2019-05-28 00:56:32 -0700

    init

---
+++ src/components/ZipInfo.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-card v-if="info">
+    <ion-card-header>
+      <ion-card-subtitle>{{info['post code']}}</ion-card-subtitle>
+      <ion-card-title>{{info['places'][0]['place name']}}</ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+      <ion-list>
+        <ion-item>
+          <ion-label>
+            <strong>State:</strong>
+            {{info['places'][0]['state']}} ({{info['places'][0]['state abbreviation']}})
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <strong>Lat:</strong>
+            {{info['places'][0]['latitude']}})
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <strong>Lon:</strong>
+            {{info['places'][0]['longitude']}})
+          </ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script>
+export default {
+  name: "ZipInfo",
+  props: ["info"]
+};
+</script>
+
+<style>
+</style>
